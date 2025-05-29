b68c8765253d7b1f22b8dc3acf4a33235d932b61
Yaseenkhan006 <24pwbcs1391@uetpeshawar.edu.pk>
2025-05-29 13:45:56 +0500

    main class

---
+++ Snake__game.java
@@ -0,0 +1,13 @@
+public import javax.swing.*;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Snake Game");
+        frame.add(new GamePanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+ 
