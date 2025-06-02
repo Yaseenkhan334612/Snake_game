2de8d234b12dcd123edb67dbc877b176c2f5aa31
Amin ul haq <amin007ulhaq@gmail.com>
2025-06-02 22:23:01 +0500

    Snake class with movement and growth logic

---
+++ Snake.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Snake {
+    private ArrayList<Point> body;
+    private String direction;
+    private boolean growing = false;
+
+   public Snake() {
+    body = new ArrayList<>();
+    body.add(new Point(5, 5)); 
+    direction = "RIGHT";
+}
+
+    public void move() {
+        Point head = body.get(0);
+        Point newHead = new Point(head);
+
+        switch (direction) {
+            case "UP": newHead.y--; break;
+            case "DOWN": newHead.y++; break;
+            case "LEFT": newHead.x--; break;
+            case "RIGHT": newHead.x++; break;
+        }
+
+        body.add(0, newHead);
+
+        if (growing) {
+            growing = false; 
+        } else {
+            body.remove(body.size() - 1);
+    }
+
+    public void grow() {
+        growing = true; 
+    }
+
+    public ArrayList<Point> getBody() {
+        return body;
+    }
+
+    public void setDirection(String dir) {
+        direction = dir;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
