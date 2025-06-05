6adcaa1c81e211e155cd87d95be811f6c5f06a2a
Yaseenkhan006 <24pwbcs1391@uetpeshawar.edu.pk>
2025-06-06 01:00:35 +0500

    SoundPlayer

---
+++ SoundPlayer.java
@@ -0,0 +1,37 @@
+import javax.sound.sampled.*;
+import java.io.File;
+
+public class SoundPlayer {
+    private Clip clip;
+
+    public void playEatSound() {
+        try {
+            if (clip == null || !clip.isRunning()) {
+                AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File("eating_sound.wav"));
+                clip = AudioSystem.getClip();
+                clip.open(audioIn);
+                clip.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopSound() {
+        if (clip != null && clip.isRunning()) {
+            clip.stop();
+            clip.close();
+        }
+    }
+
+    public void playGameStartSound() {
+        try {
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File("mgs__mp.wav"));
+            clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
